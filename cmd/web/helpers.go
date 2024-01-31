1afed3f9b7159e576c150fc6c9320a8f91a3990f
Monkeber <27787571+monkeber@users.noreply.github.com>
2024-02-01 01:32:57 +0200

    1. Added login and logout flows. 2. Added authentication and authorization.

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -31,6 +31,10 @@ func (app *application) decodePostForm(r *http.Request, dst any) error {
 	return nil
 }
 
+func (app *application) isAuthenticated(r *http.Request) bool {
+	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
+}
+
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
 	app.errorLog.Output(2, trace)
@@ -44,8 +48,9 @@ func (app *application) clientError(w http.ResponseWriter, status int) {
 
 func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
-		CurrentYear: time.Now().Year(),
-		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
+		CurrentYear:     time.Now().Year(),
+		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
+		IsAuthenticated: app.isAuthenticated(r),
 	}
 }
 
