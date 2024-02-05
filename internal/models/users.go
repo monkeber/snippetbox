dd4ee12ff4f076b10cb56c44f48709ad7e041828
Monkeber <27787571+monkeber@users.noreply.github.com>
2024-02-06 01:32:05 +0200

    1. Added protection against csrf attacks. 2. Added authenticate middleware to detect logged in and not logged in requesters.

--- internal/models/users.go
+++ internal/models/users.go
@@ -72,6 +72,12 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 	return id, nil
 }
 
-func (m *UserModel) Esists(id int) (bool, error) {
-	return false, nil
+func (m *UserModel) Exists(id int) (bool, error) {
+	var exists bool
+
+	stmt := "SELECT EXISTS(SELECT true FROM users WHERE id = ?)"
+
+	err := m.DB.QueryRow(stmt, id).Scan(&exists)
+
+	return exists, err
 }
